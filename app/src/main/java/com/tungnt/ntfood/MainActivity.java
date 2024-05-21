4b3623205cc5333551f0a229de414c7797555278
Tungdever <sidedlove03@gmail.com>
2024-05-21 19:44:56 +0700

    UI Search
    Search bar, filter

---
+++ app/src/main/java/com/tungnt/ntfood/MainActivity.java
@@ -0,0 +1,14 @@
+package com.tungnt.ntfood;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
